60579153723149087bb8a6a91418b98be2d5ca54
Joe <joecolasurdo@eltoro.com>
2020-03-30 07:07:23 -0700

    Exposing ActionStat fields for easier serialization.

--- pkg/qlearning/actionstats.go
+++ pkg/qlearning/actionstats.go
@@ -5,42 +5,42 @@ import "github.com/eltorocorp/reinforcement-learning/pkg/qlearning/iface"
 // ActionStats contains statistics about an action that has been applied to some
 // state.
 type ActionStats struct {
-	calls int
+	CallCount int
 	// QValueRaw is the raw q-value associated with this action.
-	qValueRaw float64
+	QRaw float64
 	// QValueWeighted is the q-value for this action that has been weighted
 	// according to the agent's weighting rules.
-	qValueWeighted float64
+	QWeighted float64
 }
 
 // Calls returns the number of times this action has been called.
 func (as *ActionStats) Calls() int {
-	return as.calls
+	return as.CallCount
 }
 
 // SetCalls sets the number of times this action has been called.
 func (as *ActionStats) SetCalls(n int) {
-	as.calls = n
+	as.CallCount = n
 }
 
 // QValueRaw returns the raw q-value for this action.
 func (as *ActionStats) QValueRaw() float64 {
-	return as.qValueRaw
+	return as.QRaw
 }
 
 // SetQValueRaw sets the raw q-value for this action.
 func (as *ActionStats) SetQValueRaw(value float64) {
-	as.qValueRaw = value
+	as.QRaw = value
 }
 
 // QValueWeighted returns the weighted q-value for this action.
 func (as *ActionStats) QValueWeighted() float64 {
-	return as.qValueWeighted
+	return as.QWeighted
 }
 
 // SetQValueWeighted sets the weighted q-value for this action.
 func (as *ActionStats) SetQValueWeighted(value float64) {
-	as.qValueWeighted = value
+	as.QWeighted = value
 }
 
 var _ iface.ActionStatter = (*ActionStats)(nil)
